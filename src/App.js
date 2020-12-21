3383bb595778203e1badcc4501a8056d657e5231
theerasakdodo <theerasaksolo@gmail.com>
2020-12-21 23:46:31 +0700

    routefailed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Home from './Pages/Home/index'
+import Header from './Components/Header/index'
+import { BrowserRouter, Route, Link ,NavLink} from "react-router-dom";
+import Contact from './Pages/Contact/index'
+import About from './Pages/About/index'
+
 
 function App() {
   return (
     <div >
-     <Home/>
-    
+      <Header />
+      <BrowserRouter>
+       
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/contact" component={Contact} exact={true} />
+          <Route path="/About" component={About} exact={true} />
+        
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
+
+export default () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand >
+
+          <img
+            alt=""
+            src="/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+      React Bootstrap
+
+    </Navbar.Brand>
+    
+
+      </Navbar>
+
+    </div>
+  )
+}
